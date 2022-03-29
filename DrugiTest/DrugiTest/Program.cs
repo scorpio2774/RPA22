9265427198f76b44d94105f623d7553418f48345
scorpio2774 <teoharle31@gmail.com>
2022-03-29 09:19:12 +0200

    ponovitev linq

--- DrugiTest/DrugiTest/Program.cs
+++ DrugiTest/DrugiTest/Program.cs
@@ -10,7 +10,14 @@ namespace DrugiTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("waddup bitchezzzz");
+            BazaZaVajeEntities context = new BazaZaVajeEntities();
+            //vsi dobavitelji
+            var x1 = from a in context.DOBAVITELJ
+                     select a;
+            foreach (var y in x1) {
+                Console.WriteLine(y.D_IME + " " + y.D_KONTAKT);
+            }
+
             Console.ReadLine();
         }
     }
