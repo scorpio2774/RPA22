f50755c2dc8c7e1a03721663e5b5c73f28d89f1d
scorpio2774 <teoharle31@gmail.com>
2022-04-01 09:07:43 +0200

    Povezovanje

---
+++ Povezovanje/Povezovanje/Podatki.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Povezovanje
+{
+    class Podatki
+    {
+        public int ID { get; set; }
+        public DateTime Datum { get; set; }
+        public String Ime { get; set; }
+        public double Znesek { get; set; }
+    }
+}
