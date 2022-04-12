803b22500742c7b1bfb26db94d1a5e741dc228f2
scorpio2774 <teoharle31@gmail.com>
2022-04-12 09:14:33 +0200

    vaja MVC

---
+++ VajaMVC/VajaMVC/Controllers/PredmetController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VajaMVC.Data;
+using VajaMVC.Models;
+
+namespace VajaMVC.Controllers
+{
+    public class PredmetController : Controller
+    {
+        private StudentNetContext db = new StudentNetContext();
+
+        // GET: Predmet
+        public ActionResult Index()
+        {
+            return View(db.Predmets.ToList());
+        }
+
+        // GET: Predmet/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Predmet predmet = db.Predmets.Find(id);
+            if (predmet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(predmet);
+        }
+
+        // GET: Predmet/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Predmet/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ImePredmet")] Predmet predmet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Predmets.Add(predmet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(predmet);
+        }
+
+        // GET: Predmet/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Predmet predmet = db.Predmets.Find(id);
+            if (predmet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(predmet);
+        }
+
+        // POST: Predmet/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ImePredmet")] Predmet predmet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(predmet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(predmet);
+        }
+
+        // GET: Predmet/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Predmet predmet = db.Predmets.Find(id);
+            if (predmet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(predmet);
+        }
+
+        // POST: Predmet/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Predmet predmet = db.Predmets.Find(id);
+            db.Predmets.Remove(predmet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VajaMVC/VajaMVC/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VajaMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Priimek { get; set; }
+
+    }
+}
