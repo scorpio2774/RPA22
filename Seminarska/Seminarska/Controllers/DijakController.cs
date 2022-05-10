3ad795f8c458aa23b239fad189c8c6e51170cf8c
scorpio2774 <teoharle31@gmail.com>
2022-05-10 08:46:11 +0200

    nej bi bla seminarska

---
+++ Seminarska/Seminarska/Controllers/DijakController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Seminarska.Models;
+
+namespace Seminarska.Controllers
+{
+    public class DijakController : ApiController
+    {
+        private Entities db = new Entities();
+
+        // GET: api/Dijak
+        public IQueryable<Dijak> GetDijak()
+        {
+            return db.Dijak;
+        }
+
+        // GET: api/Dijak/5
+        [ResponseType(typeof(Dijak))]
+        public IHttpActionResult GetDijak(int id)
+        {
+            Dijak dijak = db.Dijak.Find(id);
+            if (dijak == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dijak);
+        }
+
+        // PUT: api/Dijak/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDijak(int id, Dijak dijak)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != dijak.DijID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(dijak).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DijakExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Dijak
+        [ResponseType(typeof(Dijak))]
+        public IHttpActionResult PostDijak(Dijak dijak)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Dijak.Add(dijak);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = dijak.DijID }, dijak);
+        }
+
+        // DELETE: api/Dijak/5
+        [ResponseType(typeof(Dijak))]
+        public IHttpActionResult DeleteDijak(int id)
+        {
+            Dijak dijak = db.Dijak.Find(id);
+            if (dijak == null)
+            {
+                return NotFound();
+            }
+
+            db.Dijak.Remove(dijak);
+            db.SaveChanges();
+
+            return Ok(dijak);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DijakExists(int id)
+        {
+            return db.Dijak.Count(e => e.DijID == id) > 0;
+        }
+    }
+}
---
+++ Seminarska/Seminarska/Controllers/DijakPodrocjeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Seminarska.Models;
+
+namespace Seminarska.Controllers
+{
+    public class DijakPodrocjeController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: DijakPodrocje
+        public ActionResult Index()
+        {
+            var dijakPodročje = db.DijakPodročje.Include(d => d.Dijak).Include(d => d.Področja);
+            return View(dijakPodročje.ToList());
+        }
+
+        // GET: DijakPodrocje/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DijakPodročje dijakPodročje = db.DijakPodročje.Find(id);
+            if (dijakPodročje == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dijakPodročje);
+        }
+
+        // GET: DijakPodrocje/Create
+        public ActionResult Create()
+        {
+            ViewBag.DijID = new SelectList(db.Dijak, "DijID", "DijIme");
+            ViewBag.PodID = new SelectList(db.Področja, "PodID", "PodIme");
+            return View();
+        }
+
+        // POST: DijakPodrocje/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DijID,PodID,DijPodTest")] DijakPodročje dijakPodročje)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DijakPodročje.Add(dijakPodročje);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DijID = new SelectList(db.Dijak, "DijID", "DijIme", dijakPodročje.DijID);
+            ViewBag.PodID = new SelectList(db.Področja, "PodID", "PodIme", dijakPodročje.PodID);
+            return View(dijakPodročje);
+        }
+
+        // GET: DijakPodrocje/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DijakPodročje dijakPodročje = db.DijakPodročje.Find(id);
+            if (dijakPodročje == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DijID = new SelectList(db.Dijak, "DijID", "DijIme", dijakPodročje.DijID);
+            ViewBag.PodID = new SelectList(db.Področja, "PodID", "PodIme", dijakPodročje.PodID);
+            return View(dijakPodročje);
+        }
+
+        // POST: DijakPodrocje/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DijID,PodID,DijPodTest")] DijakPodročje dijakPodročje)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dijakPodročje).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DijID = new SelectList(db.Dijak, "DijID", "DijIme", dijakPodročje.DijID);
+            ViewBag.PodID = new SelectList(db.Področja, "PodID", "PodIme", dijakPodročje.PodID);
+            return View(dijakPodročje);
+        }
+
+        // GET: DijakPodrocje/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DijakPodročje dijakPodročje = db.DijakPodročje.Find(id);
+            if (dijakPodročje == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dijakPodročje);
+        }
+
+        // POST: DijakPodrocje/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DijakPodročje dijakPodročje = db.DijakPodročje.Find(id);
+            db.DijakPodročje.Remove(dijakPodročje);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
