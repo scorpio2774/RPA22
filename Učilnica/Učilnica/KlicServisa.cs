66d1de6859e4f59eea04f7e6d903e1cfa61156ad
scorpio2774 <teoharle31@gmail.com>
2022-06-10 10:07:03 +0200

    Klic api v MVVM

--- Učilnica/Učilnica/KlicServisa.cs
+++ Učilnica/Učilnica/KlicServisa.cs
@@ -1,27 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using Windows.Data.Html;
+using Windows.Storage;
 
 namespace Učilnica
 {
     class KlicServisa
     {
-        public static async Task PopulatePoglavjaAsync(ObservableCollection<Poglavja> poglavja) {
+        public static async Task PopulatePoglavjaAsync(ObservableCollection<Poglavja> poglavja)
+        {
             string url = "https://ucilnice.scng.si/webservice/rest/server.php?wstoken=cdab3d1b4fa5daac1af768a4e5030dc4&wsfunction=core_course_get_contents&courseid=110&moodlewsrestformat=json";
             List<Poglavja> vsa = new List<Poglavja>();
             //klic
-            using (var klient = new HttpClient()) {
+            using (var klient = new HttpClient())
+            {
                 HttpResponseMessage sp = await klient.GetAsync(url);
                 vsa = await sp.Content.ReadAsAsync<List<Poglavja>>();
             }
-            foreach (var p in vsa) {
+            foreach (var p in vsa)
+            {
+                p.summary = HtmlUtilities.ConvertToText(p.summary);
                 poglavja.Add(p);
             }
+        }
+        public static async Task<StorageFile> PoberiDatoteko(string imeDat, int dolžina, string ime)
+        {
+            string url = imeDat + "?token=cdab3d1b4fa5daac1af768a4e5030dc4&function=core_files_get_files";
+            HttpClient http = new HttpClient();
+            var response = await http.GetAsync(url);
+            var x = await response.Content.ReadAsStreamAsync();
+            var mapa = Windows.Storage.ApplicationData.Current.LocalFolder;
+            StorageFile obstaja = null;
+            try
+            {
+                obstaja = await mapa.GetFileAsync(ime);
+            }
+            catch { }
+            StorageFile dat = null;
+            if (obstaja != null)
+            {
+                dat = obstaja;
+                return dat;
+            }
+            dat = await mapa.CreateFileAsync(ime);
+            using (FileStream fileStream = new FileStream(dat.Path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+            {
+                await x.CopyToAsync(fileStream);
+            }
+            return dat;
         }
     }
 }
--- Učilnica/Učilnica/MainPage.xaml.cs
+++ Učilnica/Učilnica/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,9 +23,54 @@ namespace Učilnica
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public ObservableCollection<Poglavja> VsaPoglavja { get; set; }
+        public ObservableCollection<Module> Vsebina { get; set; }
         public MainPage()
         {
             this.InitializeComponent();
+            VsaPoglavja = new ObservableCollection<Poglavja>();
+            Vsebina = new ObservableCollection<Module>();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            mojP.IsActive = true;
+            mojP.Visibility = Visibility.Visible;
+            await KlicServisa.PopulatePoglavjaAsync(VsaPoglavja);
+            mojP.IsActive = false;
+            mojP.Visibility = Visibility.Collapsed;
+            masterListView.SelectedIndex = 0;
+            var selectedPoglavje = VsaPoglavja.FirstOrDefault();
+            Vsebina = selectedPoglavje.modules;
+            //ne pozabi spremeniti item source!!
+            detailListView.ItemsSource = null;
+            detailListView.ItemsSource = Vsebina;
+        }
+
+        private void masterListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var selectedPoglavje =(Poglavja) e.ClickedItem;
+            Vsebina = selectedPoglavje.modules;
+            //ne pozabi spremeniti item source!!
+            detailListView.ItemsSource = null;
+            detailListView.ItemsSource = Vsebina;
+        }
+
+        private async void detailListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var selectedModule = (Module)e.ClickedItem;
+            if (selectedModule.modname == "resource")
+            {
+                var vesContent = selectedModule.contents;
+                Content a = vesContent.FirstOrDefault();
+                string[] izbrana = a.fileurl.Split("?");
+                int dolzina = a.filesize;
+                var y = await KlicServisa.PoberiDatoteko(izbrana[0], dolzina, a.filename);
+                if (y != null)
+                {
+                    var uspeh = await Windows.System.Launcher.LaunchFileAsync(y);
+                }
+            }
         }
     }
 }
