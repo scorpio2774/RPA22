c280417395817a572f6d5e0b7471142d8644fa6e
scorpio2774 <teoharle31@gmail.com>
2022-06-10 08:41:29 +0200

    vse za nazaj

---
+++ Učilnica/Učilnica/KlicServisa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Učilnica
+{
+    class KlicServisa
+    {
+        public static async Task PopulatePoglavjaAsync(ObservableCollection<Poglavja> poglavja) {
+            string url = "https://ucilnice.scng.si/webservice/rest/server.php?wstoken=cdab3d1b4fa5daac1af768a4e5030dc4&wsfunction=core_course_get_contents&courseid=110&moodlewsrestformat=json";
+            List<Poglavja> vsa = new List<Poglavja>();
+            //klic
+            using (var klient = new HttpClient()) {
+                HttpResponseMessage sp = await klient.GetAsync(url);
+                vsa = await sp.Content.ReadAsAsync<List<Poglavja>>();
+            }
+            foreach (var p in vsa) {
+                poglavja.Add(p);
+            }
+        }
+    }
+}
