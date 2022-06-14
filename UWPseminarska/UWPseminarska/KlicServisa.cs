4b9f8c02ded92581ce1485e4d5a3a90c0489740b
scorpio2774 <teoharle31@gmail.com>
2022-06-14 09:15:55 +0200

    seminarska 2

---
+++ UWPseminarska/UWPseminarska/KlicServisa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UWPseminarska
+{
+    class KlicServisa
+    {
+        public static async Task PopulateCats(ObservableCollection<Muce> muce)
+        {
+            string url = "https://api.thecatapi.com/v1/breeds";
+            List<Muce> vseMuce = new List<Muce>();
+            using (var klient = new HttpClient())
+            {
+                HttpResponseMessage sp = await klient.GetAsync(url);
+                vseMuce= await sp.Content.ReadAsStringAsync()
+            }
+        }
+    }
+}
